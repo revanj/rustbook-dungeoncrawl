e4b1c34d0069636892af2921dace99af05e2aac9
Revan Ji <ruifanj2@illinois.edu>
2023-04-05 00:53:38 -0500

    add fov

--- src/map.rs
+++ src/map.rs
@@ -10,6 +10,7 @@ pub enum TileType {
 
 pub struct Map {
     pub tiles: Vec<TileType>,
+    pub revealed_tiles: Vec<bool>
 }
 
 pub fn map_idx(x: i32, y:i32) -> usize {
@@ -21,6 +22,7 @@ impl Map {
     pub fn new() -> Self {
         Self {
             tiles: vec![TileType::Floor; NUM_TILES],
+            revealed_tiles: vec![false; NUM_TILES]
         }
     }
 
@@ -92,5 +94,9 @@ impl BaseMap for Map {
                 self.index_to_point2d(idx1),
                 self.index_to_point2d(idx2)
             )
+    }
+
+    fn is_opaque(&self, idx: usize) -> bool {
+        self.tiles[idx as usize] != TileType::Floor
     }
 }
